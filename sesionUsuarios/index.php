2d925bc4d91665c4bf9ab4951637ba4bc5756939
juan <juan@juan-Aspire-5920.(none)>
2014-06-01 20:32:24 +0200

    Nuevo ejerciciopara ver como realizar sesiones de usuario en nuestra web

---
+++ sesionUsuarios/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Inicio de sesión</title>
+</head>
+<body>
+
+	<form action="control.php" method="post" id="form"><br>
+		Usuario: <input type="text" name="usuario" id="usuario" /><br>
+		Clave: <input type="password" name="clave" id="clave" /><br>
+		<input type="submit" value="Entrar">
+	</form>
+
+</body>
+</html>
